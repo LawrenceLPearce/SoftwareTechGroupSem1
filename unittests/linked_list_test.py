5dcee7db0f5790eb59c0a468029f00436d246287
levirumm <levirummukainen@gmail.com>
2026-05-06 09:24:37 +1000

    Added unit tests for linked list

---
+++ unittests/linked_list_test.py
@@ -0,0 +1,46 @@
+import unittest
+from DSP.linked_list import LinkedList
+
+
+def make_linked_list() -> LinkedList:
+    linked_list = LinkedList()
+    for val in [50, 30, 70, 20, 40, 60, 80]:
+        linked_list.insert(val, 0)
+    return linked_list
+        
+
+class TestInsert(unittest.TestCase):
+    def test_insert_structure(self):
+        linked_list = make_linked_list()
+        linked_list.insert(100, 2)
+        self.assertEqual(linked_list.head.next.next.data, 100)
+
+
+class TestDelete(unittest.TestCase):
+    def test_delete_structure(self):
+        linked_list = make_linked_list()
+        linked_list.delete(80)
+        self.assertEqual(linked_list.head.data, 60)
+
+
+class TestReverse(unittest.TestCase):
+    def test_reverse_structure(self):
+        linked_list = make_linked_list()
+        linked_list.reverse()
+
+        # Iterate through list and track order
+        visited = []
+        current = linked_list.head
+        while current:
+            visited.append(current.data)
+            current = current.next
+
+        self.assertEqual(visited, [50, 30, 70, 20, 40, 60, 80])
+
+
+def main():
+    unittest.main(verbosity=3)
+
+
+if __name__ == '__main__':
+    main()
